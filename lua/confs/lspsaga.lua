30cd91106b46aa72d08450fc71d1645fd478cbae
David Feng <fenghourun@gmail.com>
2022-12-30 04:31:10 -0500

    Fix some spacing

--- lua/confs/lspsaga.lua
+++ lua/confs/lspsaga.lua
@@ -4,10 +4,10 @@ if not present then return end
 
 
 lspsaga.init_lsp_saga {
-	error_sign = '', -- 
-    warn_sign = '',
-    hint_sign = '',
-    infor_sign = '',
+	error_sign = '',
+  warn_sign = '',
+  hint_sign = '',
+  infor_sign = '',
 	symbol_in_winbar = {
 		in_custom = true
 	},
