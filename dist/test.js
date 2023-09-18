f3ef483685c210946590f3364e77bf575f2e830a
David Feng <fenghourun@gmail.com>
2023-09-18 10:30:54 -0400

    fd.ignore

--- dist/test.js
+++ dist/test.js
@@ -1,1 +1,17 @@
-console.log("this shouldn't show up!j")
+let count = 0;
+
+console.log('string argument');
+
+const STUFF2 = {
+	field_a: 123,
+	field_b: "1234",
+	field_c: true,
+	field_d: (1, 2)
+}
+console.log(STUFF)
+
+for (let i = 0; i < 5; i++) {
+    console.log("got ", i);
+};
+
+console.log('done');
