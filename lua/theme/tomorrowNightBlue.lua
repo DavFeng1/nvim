0a28e057abb6f6bca81a8b0bebed1a14c79c6d2d
David Feng <fenghourun@gmail.com>
2024-06-27 13:07:51 -0400

    update color scheme

--- lua/theme/tomorrowNightBlue.lua
+++ lua/theme/tomorrowNightBlue.lua
@@ -1,66 +1,66 @@
 local M = {
-	-- Colors
-	background1 = "#0b1c39",
-	black = "#000000",
-	blue = "#BBDAFF",
-	cyan = "#99FFFF",
-	green = "#D1F1A9",
-	purple = "#EBBBFF",
-	red = "#FF9DA4",
-	light_white = "#ffffff",
-	white = "#bfbab0",
-	grey = "#a1aab4",
-	line = "#404f7d",
-	dark_green = "#238e92",
-	orange = "#ff9d9e",
-	dark_blue = "#7ab0df",
-	dark_purple = "#c678dd",
-	yellow = "#FFEEAD",
-	pink = "#C678DD",
-	lightbg = "#405570",
-	background2 = "#222650", -- darker
-	background3 = "#001127",
-	darkest_bg = '#081633',
-	statusline_bg = "#15191e",
-	pmenu_bg = "#001126",
-	pmenu_sel_bg = "#61afef",
-	folder_bg = "#86a5ff",
-	cursor_line_background = "#00346e",
+  -- Colors
+  background1 = "#0a234e",
+  background2 = "#071B3E",
+  background3 = "#051631",
+  black = "#000000",
+  blue = "#BBDAFF",
+  cyan = "#99FFFF",
+  green = "#D1F1A9",
+  purple = "#EBBBFF",
+  red = "#FF9DA4",
+  light_white = "#ffffff",
+  white = "#bfbab0",
+  grey = "#a1aab4",
+  line = "#404f7d",
+  dark_green = "#238e92",
+  orange = "#ff9d9e",
+  dark_blue = "#7ab0df",
+  dark_purple = "#c678dd",
+  yellow = "#FFEEAD",
+  pink = "#C678DD",
+  lightbg = "#405570",
+  darkest_bg = '#081633',
+  statusline_bg = "#15191e",
+  pmenu_bg = "#001126",
+  pmenu_sel_bg = "#61afef",
+  folder_bg = "#86a5ff",
+  cursor_line_background = "#00346e",
 
-	-- CMP
-	cmp_background = "#002451",
+  -- CMP
+  cmp_background = "#002451",
 
-	-- Telescope
-	telescope_background = "#001126",
+  -- Telescope
+  telescope_background = "#071B3E",
 
-	-- Lualine
-	lualine_background = "#001126",
-	lualine_background2 = "#2a75a6",
+  -- Lualine
+  lualine_background = "#071B3E",
+  lualine_background2 = "#2a75a6",
 
-	-- Bufferline
-	barbar_background = "#001733",
+  -- Bufferline
+  barbar_background = "#0a234e",
 
-	-- Which Key
-	which_key_background = "#001126",
+  -- Which Key
+  which_key_background = "#071B3E",
 
-	-- Git Signs
-	git_signs_add = "#487e02",
-	git_signs_blame = "#35577d",
+  -- Git Signs
+  git_signs_add = "#487e02",
+  git_signs_blame = "#35577d",
 
-	-- Diagnostics
-	diagnostics_error = "#f14c4c",
+  -- Diagnostics
+  diagnostics_error = "#f14c4c",
 
-	-- Neo Tree
-	neo_tree_background = "#001126",
-	neo_tree_modified = "#e2a974",
-	neo_tree_root_background = "#1a304d",
-	neo_tree_winbar_background = "#1a304d",
+  -- Neo Tree
+  neo_tree_background = "#071B3E",
+  neo_tree_modified = "#e2a974",
+  neo_tree_root_background = "#1a304d",
+  neo_tree_winbar_background = "#1a304d",
 
-	-- Toggle Tem
-	toggle_term_background = "#001c40",
+  -- Toggle Tem
+  toggle_term_background = "#001c40",
 
-	-- Winbar
-	winbar_background = "#002451",
+  -- Winbar
+  winbar_background = "#002451",
 }
 
 return M
