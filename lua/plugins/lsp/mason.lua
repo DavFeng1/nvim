91dcd91d4361f18a3a45d46bea147586215f4347
David Feng <fenghourun@gmail.com>
2024-09-27 00:41:11 -0400

    Ensure installed

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,6 +1,6 @@
 return {
   "williamboman/mason.nvim",
   opts = {
-    ensure_installed = { "lua_ls", "rust_analyzer", "taplo", "dockerls" }
+    ensure_installed = { "lua_ls", "rust_analyzer", "taplo", "dockerls", "biome", "typescript-language-server" }
   }
 }
