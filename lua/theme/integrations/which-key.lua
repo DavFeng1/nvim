d5db95893773467a6c011a8fefecefc4bf3132d5
David Feng <fenghourun@gmail.com>
2024-11-15 13:48:09 -0500

    Use text-primary for whichkey

--- lua/theme/integrations/which-key.lua
+++ lua/theme/integrations/which-key.lua
@@ -2,7 +2,7 @@ local get_colors = require "theme.colors"
 
 return function()
   local colors = get_colors()
-  vim.api.nvim_set_hl(0, "WhichKey", { fg = colors.light_white, bg = colors.which_key_background })
+  vim.api.nvim_set_hl(0, "WhichKey", { fg = colors.text_primary, bg = colors.which_key_background })
   vim.api.nvim_set_hl(0, "WhichKeyFloat", { fg = colors.white, bg = colors.which_key_background })
   vim.api.nvim_set_hl(0, "WhichKeyNormal", { fg = colors.white, bg = colors.which_key_background })
   vim.api.nvim_set_hl(0, "WhichKeyDesc", { fg = colors.red, bg = colors.which_key_background })
